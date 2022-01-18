3453a4854a276bc8d71a02b55b404e2311f319ef
koojina <70617718+koojina@users.noreply.github.com>
2022-01-19 01:42:44 +0900

    Main_2(AngerGauge,Meter,Score)

--- Assets/04.Scripts/Scripts_G/GameManager_G.cs
+++ Assets/04.Scripts/Scripts_G/GameManager_G.cs
@@ -7,10 +7,20 @@ public class GameManager_G : MonoBehaviour
 {
     public Slider angerSlider;
     public Image angerFill;
+    public Text meterText;
+    public Text scoreText;
 
-    private int curTime;
-    private int time;
+    public float meter;
+    public int score;
+
+    [SerializeField]
+    private float pastMeter = 0;
+
+    //[SerializeField]
+    //private int time;
+
     // Start is called before the first frame update
+
     void Start()
     {
         angerSlider.value = 100; //G: 0으로 초기화 해서 시작할 것 
@@ -19,7 +29,9 @@ public class GameManager_G : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MeterUpdate();
         AngerUpdate();
+        
 
     }
 
@@ -34,5 +46,25 @@ public class GameManager_G : MonoBehaviour
         { }
                 //G: 궁극기 써지는 부분
         
+    }
+
+    private void MeterUpdate() //G:미터기 업데이트 추후 수정
+    {
+        meter += Time.deltaTime / 20;
+        meterText.text = meter.ToString("F2") + "m";
+       
+        ScoreUpdate();
+
+    }
+
+    private void ScoreUpdate()  //G: 점수 업데이트 (미터에 따른 점수 추가 O | 아이템에 따른 점수 추가 X | 장애물에 따른 점수 감소 X)
+    {
+        if (meter - pastMeter >= 1f)
+        {
+            pastMeter = meter;
+            score += 50;
+            scoreText.text = score.ToString();
+            
+        }
     }
 }
