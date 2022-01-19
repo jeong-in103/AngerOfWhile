00d661337d744e43acebc2521aa2602120eb22b4
20201312 문정인 <83389445+jeong-in103@users.noreply.github.com>
2022-01-19 09:22:07 +0900

    FAET Enemy - Mot, Ship

--- Assets/04.Scripts/Scripts_M/Scriptable/Ship.cs
+++ Assets/04.Scripts/Scripts_M/Scriptable/Ship.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Ship", menuName = "New Object/Ship")]
 public class Ship : ScriptableObject  // 스크립터블 오브젝트
 {
-    public enum ShipType  // 배 유형
-    {
-        SHIP,
-        SUB, 
-        MOT, 
-        HUNT, 
-        NAVAL, 
-    }
-
-    [SerializeField]
-    public ShipType type; // 배 유형
-    public ShipType Type { get { return type; } }
-
     [SerializeField] 
     private float moveSpeed; // 속도
     public float MoveSpeed { get { return moveSpeed; } }
@@ -25,4 +12,16 @@ public class Ship : ScriptableObject  // 스크립터블 오브젝트
     [SerializeField]
     private float angerGauge; // 분노게이지
     public float AngerGauge { get { return angerGauge; } }
+
+    [SerializeField]
+    private float score; // 추가 점수
+    public float Score { get { return score; } }
+
+    [SerializeField]
+    private GameObject leaveObject; // 배가 남기고 가는 오브젝트
+    public GameObject LeaveObject { get { return leaveObject; } }
+
+    [SerializeField]
+    private GameObject bulletObject; // 배가 발사하는 총알 오브젝트
+    public GameObject BulletObject { get { return bulletObject; } }
 }
